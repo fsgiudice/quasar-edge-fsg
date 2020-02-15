655ff2e859343fa289a7e6e398eeedd445a934c5
fsgiudice <info@fsgiudice.com>
2020-02-15 04:38:54 +0100

    updated

--- dist/quasar.esm.js
+++ dist/quasar.esm.js
@@ -1093,74 +1093,265 @@ var QInputFrame = {render: function(){var _vm=this;var _h=_vm.$createElement;var
   }
 };
 
-function getSize (el) {
+var listenOpts = {};
+Object.defineProperty(listenOpts, 'passive', {
+  configurable: true,
+  get: function get () {
+    var passive;
+
+    try {
+      var opts = Object.defineProperty({}, 'passive', {
+        get: function get () {
+          passive = { passive: true };
+        }
+      });
+      window.addEventListener('qtest', null, opts);
+      window.removeEventListener('qtest', null, opts);
+    }
+    catch (e) {}
+
+    listenOpts.passive = passive;
+    return passive
+  },
+  set: function set (val) {
+    Object.defineProperty(this, 'passive', {
+      value: val
+    });
+  }
+});
+
+function getEvent (e) {
+  return e || window.event
+}
+
+function rightClick (e) {
+  e = getEvent(e);
+
+  if (e.which) {
+    return e.which === 3
+  }
+  if (e.button) {
+    return e.button === 2
+  }
+
+  return false
+}
+
+function getEventKey (e) {
+  e = getEvent(e);
+  return e.which || e.keyCode
+}
+
+function position (e) {
+  var posx, posy;
+  e = getEvent(e);
+
+  if (e.touches && e.touches[0]) {
+    e = e.touches[0];
+  }
+  else if (e.changedTouches && e.changedTouches[0]) {
+    e = e.changedTouches[0];
+  }
+
+  if (e.clientX || e.clientY) {
+    posx = e.clientX;
+    posy = e.clientY;
+  }
+  else if (e.pageX || e.pageY) {
+    posx = e.pageX - document.body.scrollLeft - document.documentElement.scrollLeft;
+    posy = e.pageY - document.body.scrollTop - document.documentElement.scrollTop;
+  }
+
   return {
-    width: el.offsetWidth,
-    height: el.offsetHeight
+    top: posy,
+    left: posx
   }
 }
 
-var QResizeObservable = {render: function(){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticStyle:{"z-index":"-1"},style:(_vm.absoluteFull)},[_c('div',{ref:"expand",style:(_vm.absoluteFull),on:{"scroll":_vm.onResize}},[_c('div',{ref:"expandChild",staticStyle:{"width":"100000px","height":"100000px"},style:(_vm.absoluteTop)})]),_vm._v(" "),_c('div',{ref:"shrink",style:(_vm.absoluteFull),on:{"scroll":_vm.onResize}},[_c('div',{staticStyle:{"width":"200%","height":"200%"},style:(_vm.absoluteTop)})])])},staticRenderFns: [],
-  name: 'q-resize-observable',
-  data: function () { return ({
-    absoluteFull: {
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 0,
-      overflow: 'hidden',
-      visibility: 'hidden'
-    },
-    absoluteTop: {
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      transition: '0s'
+function targetElement (e) {
+  var target;
+  e = getEvent(e);
+
+  if (e.target) {
+    target = e.target;
+  }
+  else if (e.srcElement) {
+    target = e.srcElement;
+  }
+
+  // defeat Safari bug
+  if (target.nodeType === 3) {
+    target = target.parentNode;
+  }
+
+  return target
+}
+
+// Reasonable defaults
+var PIXEL_STEP = 10;
+var LINE_HEIGHT = 40;
+var PAGE_HEIGHT = 800;
+
+function getMouseWheelDistance (e) {
+  var
+    sX = 0, sY = 0, // spinX, spinY
+    pX = 0, pY = 0; // pixelX, pixelY
+
+  // Legacy
+  if ('detail' in e) { sY = e.detail; }
+  if ('wheelDelta' in e) { sY = -e.wheelDelta / 120; }
+  if ('wheelDeltaY' in e) { sY = -e.wheelDeltaY / 120; }
+  if ('wheelDeltaX' in e) { sX = -e.wheelDeltaX / 120; }
+
+  // side scrolling on FF with DOMMouseScroll
+  if ('axis' in e && e.axis === e.HORIZONTAL_AXIS) {
+    sX = sY;
+    sY = 0;
+  }
+
+  pX = sX * PIXEL_STEP;
+  pY = sY * PIXEL_STEP;
+
+  if ('deltaY' in e) { pY = e.deltaY; }
+  if ('deltaX' in e) { pX = e.deltaX; }
+
+  if ((pX || pY) && e.deltaMode) {
+    if (e.deltaMode === 1) { // delta in LINE units
+      pX *= LINE_HEIGHT;
+      pY *= LINE_HEIGHT;
     }
-  }); },
+    else { // delta in PAGE units
+      pX *= PAGE_HEIGHT;
+      pY *= PAGE_HEIGHT;
+    }
+  }
+
+  // Fall-back if spin cannot be determined
+  if (pX && !sX) { sX = (pX < 1) ? -1 : 1; }
+  if (pY && !sY) { sY = (pY < 1) ? -1 : 1; }
+
+  /*
+   * spinX  -- normalized spin speed (use for zoom) - x plane
+   * spinY  -- " - y plane
+   * pixelX -- normalized distance (to pixels) - x plane
+   * pixelY -- " - y plane
+   */
+  return {
+    spinX: sX,
+    spinY: sY,
+    pixelX: pX,
+    pixelY: pY
+  }
+}
+
+
+var event = Object.freeze({
+	listenOpts: listenOpts,
+	rightClick: rightClick,
+	getEventKey: getEventKey,
+	position: position,
+	targetElement: targetElement,
+	getMouseWheelDistance: getMouseWheelDistance
+});
+
+var QResizeObservable = {
+  name: 'QResizeObservable',
+  props: {
+    debounce: {
+      type: Number,
+      default: 100
+    }
+  },
+  data: function data () {
+    return this.hasObserver
+      ? {}
+      : { url: this.$q.platform.is.ie ? null : 'about:blank' }
+  },
   methods: {
     onResize: function onResize () {
-      var size = getSize(this.$el.parentNode);
+      this.timer = null;
+
+      if (!this.$el || !this.$el.parentNode) {
+        return
+      }
+
+      var
+        parent = this.$el.parentNode,
+        size = {
+          width: parent.offsetWidth,
+          height: parent.offsetHeight
+        };
 
       if (size.width === this.size.width && size.height === this.size.height) {
         return
       }
 
-      if (!this.timer) {
-        this.timer = window.requestAnimationFrame(this.emit);
-      }
-
       this.size = size;
-    },
-    emit: function emit () {
-      this.timer = null;
-      this.reset();
       this.$emit('resize', this.size);
     },
-    reset: function reset () {
-      var ref = this.$refs;
-      if (ref.expand) {
-        ref.expand.scrollLeft = 100000;
-        ref.expand.scrollTop = 100000;
+    trigger: function trigger (immediately) {
+      if (immediately || this.debounce === 0) {
+        this.onResize();
       }
-      if (ref.shrink) {
-        ref.shrink.scrollLeft = 100000;
-        ref.shrink.scrollTop = 100000;
+      else if (!this.timer) {
+        this.timer = setTimeout(this.onResize, this.debounce);
       }
     }
   },
-  mounted: function mounted () {
+  render: function render (h) {
     var this$1 = this;
 
-    this.$nextTick(function () {
-      this$1.size = {};
-      this$1.onResize();
-    });
+    if (this.hasObserver) {
+      return
+    }
+
+    return h('object', {
+      style: this.style,
+      attrs: {
+        type: 'text/html',
+        data: this.url,
+        'aria-hidden': true
+      },
+      on: {
+        load: function () {
+          this$1.$el.contentDocument.defaultView.addEventListener('resize', this$1.trigger, listenOpts.passive);
+          this$1.trigger(true);
+        }
+      }
+    })
+  },
+  beforeCreate: function beforeCreate () {
+    this.size = { width: -1, height: -1 };
+    this.hasObserver = typeof ResizeObserver !== 'undefined';
+
+    if (!this.hasObserver) {
+      this.style = (this.$q.platform.is.ie ? 'visibility:hidden;' : '') + "display:block;position:absolute;top:0;left:0;right:0;bottom:0;height:100%;width:100%;overflow:hidden;pointer-events:none;z-index:-1;";
+    }
+  },
+  mounted: function mounted () {
+    if (this.hasObserver) {
+      this.observer = new ResizeObserver(this.trigger);
+      this.observer.observe(this.$el.parentNode);
+      return
+    }
+
+    this.trigger(true);
+
+    if (this.$q.platform.is.ie) {
+      this.url = 'about:blank';
+    }
   },
   beforeDestroy: function beforeDestroy () {
-    window.cancelAnimationFrame(this.timer);
-    this.$emit('resize', {width: 0, height: 0});
+    clearTimeout(this.timer);
+
+    if (this.hasObserver) {
+      this.observer.unobserve(this.$el.parentNode);
+      return
+    }
+
+    if (this.$el.contentDocument) {
+      this.$el.contentDocument.defaultView.removeEventListener('resize', this.trigger, listenOpts.passive);
+    }
   }
 };
 
@@ -1337,6 +1528,8 @@ var QWindowResizeObservable = {
     window.removeEventListener('resize', this.trigger);
   }
 };
+
+// import QResizeObservable from './QResizeObservable.vue'
 
 var mixin = {
   props: {
@@ -1713,139 +1906,6 @@ function extend () {
 
   return target
 }
-
-function getEvent (e) {
-  return e || window.event
-}
-
-function rightClick (e) {
-  e = getEvent(e);
-
-  if (e.which) {
-    return e.which === 3
-  }
-  if (e.button) {
-    return e.button === 2
-  }
-
-  return false
-}
-
-function getEventKey (e) {
-  e = getEvent(e);
-  return e.which || e.keyCode
-}
-
-function position (e) {
-  var posx, posy;
-  e = getEvent(e);
-
-  if (e.touches && e.touches[0]) {
-    e = e.touches[0];
-  }
-  else if (e.changedTouches && e.changedTouches[0]) {
-    e = e.changedTouches[0];
-  }
-
-  if (e.clientX || e.clientY) {
-    posx = e.clientX;
-    posy = e.clientY;
-  }
-  else if (e.pageX || e.pageY) {
-    posx = e.pageX - document.body.scrollLeft - document.documentElement.scrollLeft;
-    posy = e.pageY - document.body.scrollTop - document.documentElement.scrollTop;
-  }
-
-  return {
-    top: posy,
-    left: posx
-  }
-}
-
-function targetElement (e) {
-  var target;
-  e = getEvent(e);
-
-  if (e.target) {
-    target = e.target;
-  }
-  else if (e.srcElement) {
-    target = e.srcElement;
-  }
-
-  // defeat Safari bug
-  if (target.nodeType === 3) {
-    target = target.parentNode;
-  }
-
-  return target
-}
-
-// Reasonable defaults
-var PIXEL_STEP = 10;
-var LINE_HEIGHT = 40;
-var PAGE_HEIGHT = 800;
-
-function getMouseWheelDistance (e) {
-  var
-    sX = 0, sY = 0, // spinX, spinY
-    pX = 0, pY = 0; // pixelX, pixelY
-
-  // Legacy
-  if ('detail' in e) { sY = e.detail; }
-  if ('wheelDelta' in e) { sY = -e.wheelDelta / 120; }
-  if ('wheelDeltaY' in e) { sY = -e.wheelDeltaY / 120; }
-  if ('wheelDeltaX' in e) { sX = -e.wheelDeltaX / 120; }
-
-  // side scrolling on FF with DOMMouseScroll
-  if ('axis' in e && e.axis === e.HORIZONTAL_AXIS) {
-    sX = sY;
-    sY = 0;
-  }
-
-  pX = sX * PIXEL_STEP;
-  pY = sY * PIXEL_STEP;
-
-  if ('deltaY' in e) { pY = e.deltaY; }
-  if ('deltaX' in e) { pX = e.deltaX; }
-
-  if ((pX || pY) && e.deltaMode) {
-    if (e.deltaMode === 1) { // delta in LINE units
-      pX *= LINE_HEIGHT;
-      pY *= LINE_HEIGHT;
-    }
-    else { // delta in PAGE units
-      pX *= PAGE_HEIGHT;
-      pY *= PAGE_HEIGHT;
-    }
-  }
-
-  // Fall-back if spin cannot be determined
-  if (pX && !sX) { sX = (pX < 1) ? -1 : 1; }
-  if (pY && !sY) { sY = (pY < 1) ? -1 : 1; }
-
-  /*
-   * spinX  -- normalized spin speed (use for zoom) - x plane
-   * spinY  -- " - y plane
-   * pixelX -- normalized distance (to pixels) - x plane
-   * pixelY -- " - y plane
-   */
-  return {
-    spinX: sX,
-    spinY: sY,
-    pixelX: pX,
-    pixelY: pY
-  }
-}
-
-
-var event = Object.freeze({
-	rightClick: rightClick,
-	getEventKey: getEventKey,
-	position: position,
-	targetElement: targetElement,
-	getMouseWheelDistance: getMouseWheelDistance
-});
 
 function getAnchorPosition (el, offset) {
   var ref = el.getBoundingClientRect();
